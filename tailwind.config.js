6d8fadbdbc0da972b544b3cd85b4bfe811ba67d5
gthell <sosxradar@gmail.com>
2024-05-30 04:17:52 +0700

    Add flowbite UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.vue",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
