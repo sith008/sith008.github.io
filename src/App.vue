848bf636e9cb7a0f72ff76f1f06ca36fecf5f4dd
gthell <sosxradar@gmail.com>
2024-05-30 23:26:58 +0700

    Update height of app to h-full

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import TableView from "./components/TableView.vue";
 </script>
 
 <template>
-  <div class="container mx-auto px-4">
+  <div class="container mx-auto px-4 h-screen overflow-hidden">
     <h1 class="text-2xl my-10 font-semibold text-gray-900 dark:text-white">
       Countries Catalog
     </h1>
